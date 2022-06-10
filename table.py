aec2a339d3e233771bbfe088bc048484c5b1d607
yzhao <ying.zhao@sfchronicle.comm>
2022-06-10 14:16:36 -0700

    get all tables from a super messy pdf

--- table.py
+++ table.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import camelot.io as camelot
 
-tables = camelot.read_pdf('MON2_RI_01_table.pdf', flavor = 'stream',pages='all')
+
+# get all tables from pdf
 dfs = []
-for table in tables:
-  df = table.df
-  dfs.append(df)
+for page in list(range(1,396)):
+  try:
+    table = camelot.read_pdf('pdf/super_messy_MON1_RISI_01.pdf', flavor = 'stream',pages=f'{page}',strip_text= '\n')
 
+    if len(table)  == 1:
+      df_table = table[0].df
+    else:
+      df_table = table.df
+  except:
+    pass
+# for table in tables:
+  dfs.append(df_table)
 df_all= pd.concat(dfs, ignore_index = True)
-df_all.to_csv('mon2_ri_01_table.csv')
 
+df_all.to_csv('csv/super_messy_mon2_risi_01_table.csv')
 
 
