c501317575a497293ebd0e0fbc5218063eef7253
yzhao <ying.zhao@sfchronicle.comm>
2022-06-12 02:04:29 -0700

    working on syr

--- text_config.py
+++ text_config.py
@@ -1,80 +1,50 @@
 text = {
   "syr": [
-    { "doc_type":"RI", # restraint incident records 
-    "incident_date":"3/27/19",
-    "time_began":"10:00am",
-    "time_ended":"10:05am",
-    "nature_restraint":"Arm restract per CPI training",
-    "location_restraint":"Room",
-    "staff":"Karn Hoke t Msa bihlerest Witness",
-    "description":"running through School",
-    "behavior":{
-          "Imminent serious physical harm to themselves":"yes", # this part might need to be filled manually
-          "Imminent serious physical harm to others":"yes",
-          "Imminent serious physical harm to themselves and others":"no",
-          "Imminent serious property destruction":"no",
-          "Imminent serious physical harm to themselves and imminent serious property destruction":"no",
-          "Imminent serious physical harm to others and imminent serious property destruction":"no",
-          "Imminent serious physical harm to themselves and others and imminent serious property destruction":"no"
-    },
-    "explaination_behaviors":"Spitting in teachers face Verbally agnalium Swearing. engagens other the Student situation: tegung to fight. Kicking me sweaing",
-    "efforts_to_deescalte_situation":{
-          "provided choices":"no",
-          "reduced demands explain":"no",
-          "verbal redirection":"no",
-          "reduced verbal interaction":"no",
-          "calming techniques":"no",
-          "other":"no"
-    },
-    "alternatives_to_restraint":{
-          "removal of other students":"yes",
-          "requests for assistance":"no",
-          "other":"no",
-          "voluntary removal of students to another location": "no",
-          "explaination":"no"
-    },
-    "observations_after_restraint":"student war Cala went to Calming Corner",
-    "any_injuries":"STUDENT was SEEN YESENDAY of PAIN in R FOREARM. 4/v SAME TODAY- MSM CAUSED TO SCEK MARA CANSUCT. Arron N MARIAS/ swim MITED"
+    { "doc_type":"Written Summary of Staff/Student Restraint Use Procedures", 
+    'school':'FROM-',
+    "incident_date": "Date of restraint",
+    "time_began": "Time began",
+    "time_ended": "Time ended",
+    "restraint_type": "Nature of restraint (describe type of physical restraint used)",
+    "staff":"Name(s) of staff member(s) adminstering restraint",
+    "incident_description":"Explain student behavior(s) that prompted physical restraint",
+    "reason":"Explain student behavior(s) that prompted physical restraint",
+    "injury":"Note any injuries to student and/or staff that may have occured"
     },
     
     {
-      "doc_type":"II", # internal investigations
-      "incident_date":"3/23/2019",
-      "report_date":"3/23/2019",
-      "person_giving_testimony":"Jonge Didson (Sentry)",
-      "dob_person_giving_testimony":"NA",
-      "home_address":"NA",
-      "job_if_not_student":"Sentry",
-      "willing_to_testify":"yes",
-      "report_of_incident":"On this day March 26, 2019 at 9:40AM. I was called to room (Room for clession cossistence Upen acrival to the clession . noticed standing at in the hall across from the clession. Mc. Dreen had cok watch iPT can take ,as well as issue de for a quick mediation as they seemed to DE having an exited the clessroom, in down the Students to I sent to the Bee room till I returned with made it to Bec Writer found at the bollom of starts next to abone that feed to North parking lot. Writer then walked with up to Beceipen entry to BIC, Mr. Rodriguez ask to wait of in the hallwall appeared to be more upset Soi stayed in Bvs As exited  and ast him unsen des aftert with his right hand. Student was then placed in became very aggrestive Ma Rodrigues, a certraint (See Restraint report) Mr. Day inces called, took Student to his office Restraint began at 9:50am. ended 9:55am. was checked by nurse. See reshaint report for nurse notes "
+      "doc_type":["INCIDENT REPORT FORM",'INVESTIGATION REPORT'],
+      'school':'FROM-',
+      "incident_date":"DATE/TIME OF INCIDENT",
+      "report_date":"DATE/TIME INCIDENT REPORT",
+      "staff":"NAME OF PERSON GIVING TESTIMONY",
+      "incident_description":'TO THE PRINCIPAL'
     },
     {
-      'doc_type':'PRD', # Physical Restraint Documentation Form. Note: this is not listed as a type of forms in the documentation
-      'time_duration':'8 minutes',
-      'staff':'Ken Klannost, Done kine ',
-      'restraint_used':' ',
-      'school_sentry_resource_involved':{
-        'yes': '',
-        'no': 'yes',
-        'name': ''
-      },
-      'description_location':'Clissican', # I think it's classroom
-      'anyone_observe':{
-        'yes': 'yes',
-        'no': '',
-        'name': 'Michella nixr '
-      },
-      'parents_notified':'',
-      'form_completed_by':'',
-      'position':'',
-      'nurse':'w',
-      'time':'10:15',
-      'medical_findings':'small red manes on both forearms in the shape of fingerprints. Mild reaness. ND bleeding or brising at this time. stated thatleft shoulder nust. ICC Pack applied. said hit her head on the small floor bump Also from right runing of aware. it firenead. around. on side Ice Pack. caplied. mom aware',
-      'signature':'Anthony ',
-      'nurse_signature':'Laun walf',
-      'administrator_signature':'his Up'
+      'doc_type':"Incident Report Form",
+      'incident_date':'Date of Incident',
+      'incident_time':'Time',
+      'incident_description':'NARRATIVE',
+      'injury':'Information known about the resulting injury(ies)'
     },
-    {},
+    
+    {
+      'doc_type':'Physical Restraint Incident Documentation Form', # Physical Restraint Documentation Form. Note: this is not listed as a type of forms in the documentation
+      'duration':'Time/Duration of Physical Restraint',
+      'reason':'Circumstances that required physical restraint',
+      'staff':'Staff involved in the physical restraint',
+      'restraint_description':'Description and location of physical restraint',
+      'injure':'Medical findings (regarding student and staff)',
+      'incident_date':'Date'
+    },
+    {
+      'doc_type':'Physcial Restraint Documentation Form',
+      'duration':'Time/Duration of Physical Restraint',
+      'staff':'Staff involved in the Physical restraint',
+      'restraint_type':'Which restraint was used (Please describe the restraint)',
+      'location':'Description and location of physical restraint',
+      'injury':'Medical findings (regarding student and staff)'
+    }
   ],
 
   "mon1" : [
