bb0fa6e01067cf7b5825c25b0e0884f4d2395244
yzhao <ying.zhao@sfchronicle.comm>
2022-07-06 08:45:59 -0700

    get tables

--- table.py
+++ table.py
@@ -1,27 +1,111 @@
-import pandas as pd
-import camelot.io as camelot
+import webbrowser, os
+import json
+import boto3
+import io
+from io import BytesIO
+import sys
+from pprint import pprint
 
 
-# get all tables from pdf
-# dfs = []
-# for page in list(range(1,396)):
-#   try:
-#     table = camelot.read_pdf('pdf/super_messy_MON1_RISI_01.pdf', flavor = 'stream',pages=f'{page}',strip_text= '\n')
-
-#     if len(table)  == 1:
-#       df_table = table[0].df
-#     else:
-#       df_table = table.df
-#   except:
-#     pass
-# # for table in tables:
-#   dfs.append(df_table)
-# df_all= pd.concat(dfs, ignore_index = True)
-
-# df_all.to_csv('csv/super_messy_mon2_risi_01_table.csv')
-
-table = camelot.read_pdf('pdf/BETH_SI_01_table.pdf', flavor = 'stream')
+def get_rows_columns_map(table_result, blocks_map):
+    rows = {}
+    for relationship in table_result['Relationships']:
+        if relationship['Type'] == 'CHILD':
+            for child_id in relationship['Ids']:
+                cell = blocks_map[child_id]
+                if cell['BlockType'] == 'CELL':
+                    row_index = cell['RowIndex']
+                    col_index = cell['ColumnIndex']
+                    if row_index not in rows:
+                        # create new row
+                        rows[row_index] = {}
+                        
+                    # get the text value
+                    rows[row_index][col_index] = get_text(cell, blocks_map)
+    return rows
 
 
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] =='SELECTED':
+                            text +=  'X '    
+    return text
 
-print(table[0].df)
+
+def get_table_csv_results(file_name):
+
+    with open(file_name, 'rb') as file:
+        img_test = file.read()
+        bytes_test = bytearray(img_test)
+        print('Image loaded', file_name)
+
+    # process using image bytes
+    # get the results
+    client = boto3.client('textract', region_name='us-east-1')
+
+    response = client.analyze_document(Document={'Bytes': bytes_test}, FeatureTypes=['TABLES'])
+
+    # Get the text blocks
+    blocks=response['Blocks']
+    pprint(blocks)
+
+    blocks_map = {}
+    table_blocks = []
+    for block in blocks:
+        blocks_map[block['Id']] = block
+        if block['BlockType'] == "TABLE":
+            table_blocks.append(block)
+
+    if len(table_blocks) <= 0:
+        return "<b> NO Table FOUND </b>"
+
+    csv = ''
+    for index, table in enumerate(table_blocks):
+        csv += generate_table_csv(table, blocks_map, index +1)
+        csv += '\n\n'
+
+    return csv
+
+def generate_table_csv(table_result, blocks_map, table_index):
+    rows = get_rows_columns_map(table_result, blocks_map)
+
+    table_id = 'Table_' + str(table_index)
+    
+    # get cells.
+    csv = 'Table: {0}\n\n'.format(table_id)
+
+    for row_index, cols in rows.items():
+        
+        for col_index, text in cols.items():
+            csv += '{}'.format(text) + ","
+        csv += '\n'
+        
+    csv += '\n\n\n'
+    return csv
+
+def main(file_name):
+    table_csv = get_table_csv_results(file_name)
+
+    output_file = 'output.csv'
+
+    # replace content
+    with open(output_file, "wt") as fout:
+        fout.write(table_csv)
+
+    # show the results
+    print('CSV OUTPUT FILE: ', output_file)
+
+
+file_name = "pdf/ROC_RICPIPR_01_table_01.pdf"
+
+
+main(file_name)
+
